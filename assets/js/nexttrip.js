fbe7dcc00913dd2d5bba51d4c295b2fefe372bdb
crustopher <67013495+crustopher@users.noreply.github.com>
2020-08-28 18:00:44 +0930

    Typo

--- assets/js/nexttrip.js
+++ assets/js/nexttrip.js
@@ -11,7 +11,7 @@ var next;
 var today = new Date();
 
 trips[0][0] = new Date(2020, 8, 13)
-trips[0][1] = "<b>Willangi</b><br />12th - 13th September 2020, 2020<br />Trip Leader: Aaron "
+trips[0][1] = "<b>Willangi</b><br />12th - 13th September 2020<br />Trip Leader: Aaron "
 trips[1][0] = new Date(2020, 8, 27)
 trips[1][1] = "<b>Barossa</b><br />26th - 27th September, 2020<br />Trip Leader: Neil "
 trips[2][0] = new Date(2020, 9, 5)
